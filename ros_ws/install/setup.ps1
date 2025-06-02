576b8f8504a2690a68240d78a4499a0dcca4723e
1402nikhil <200414nikhiljagdale@gmail.com>
2025-06-02 12:04:50 +0530

    build, install and log directory

---
+++ ros_ws/install/setup.ps1
@@ -0,0 +1,38 @@
+# generated from colcon_powershell/shell/template/prefix_chain.ps1.em
+
+# This script extends the environment with the environment of other prefix
+# paths which were sourced when this file was generated as well as all packages
+# contained in this prefix path.
+
+# function to source another script with conditional trace output
+# first argument: the path of the script
+function _colcon_prefix_chain_powershell_source_script {
+  param (
+    $_colcon_prefix_chain_powershell_source_script_param
+  )
+  # source script with conditional trace output
+  if (Test-Path $_colcon_prefix_chain_powershell_source_script_param) {
+    if ($env:COLCON_TRACE) {
+      echo ". '$_colcon_prefix_chain_powershell_source_script_param'"
+    }
+    . "$_colcon_prefix_chain_powershell_source_script_param"
+  } else {
+    Write-Error "not found: '$_colcon_prefix_chain_powershell_source_script_param'"
+  }
+}
+
+# source chained prefixes
+_colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/nikhil/Work/ROS/ros2_ws/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/nikhil/Work/ROS/ros2_ws_1/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/nikhil/Work/ROS/turtlebot3_ws/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/nikhil/Work/ROS/controller_ws/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/nikhil/Work/ROS/control_ws/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/nikhil/Work/ROS/microRos_ws/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/nikhil/Work/dev_ws/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/nikhil/Work/ROS/rplidar_ws/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/nikhil/Work/ros2/basketball_bot/install\local_setup.ps1"
+
+# source this prefix
+$env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
+_colcon_prefix_chain_powershell_source_script "$env:COLCON_CURRENT_PREFIX\local_setup.ps1"
